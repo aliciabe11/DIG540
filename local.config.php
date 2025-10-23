3ba5a706d7f2573b71742530914dbe267383e65b
aliciabe11 <alicia.bell@maine.edu>
2025-10-23 04:33:57 -0400

    Create local.config.php

---
+++ local.config.php
@@ -0,0 +1,33 @@
+<?php
+return [
+    'logger' => [
+        'log' => false,
+        'priority' => \Laminas\Log\Logger::NOTICE,
+    ],
+    'http_client' => [
+        'sslcapath' => null,
+        'sslcafile' => null,
+    ],
+    'cli' => [
+        'phpcli_path' => '/usr/local/bin/php',
+    ],
+    'thumbnails' => [
+        'types' => [
+            'large' => ['constraint' => 800],
+            'medium' => ['constraint' => 200],
+            'square' => ['constraint' => 200],
+        ],
+        'thumbnailer_options' => [
+            'imagemagick_dir' => '/usr/bin/',
+        ],
+    ],
+    'translator' => [
+        'locale' => 'en_US',
+    ],
+    'service_manager' => [
+        'aliases' => [
+            'Omeka\File\Store' => 'Omeka\File\Store\Local',
+            'Omeka\File\Thumbnailer' => 'Omeka\File\GdThumbnailer',
+        ],
+    ],
+];
